3022d233c3fd3dd26eb2dba38ce9b4c3d24fab3f
William-Fonseca-Geralde <127157689+William-Fonseca-Geralde@users.noreply.github.com>
2024-10-09 20:07:24 -0300

    Adicionado uma classe no Views

--- DomainTrader/core/views.py
+++ DomainTrader/core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import View
+import requests
 
 # Create your views here.
 class IndexView(View):
@@ -9,3 +10,15 @@ class IndexView(View):
 class ApiTesteView(View):
   def get(self, request):
     return render(request, 'apiTeste.html')
+  
+  def apiCall(request):
+    url = 'https://whois.whoisxmlapi.com/signup'
+    
+    response = request.get(url)
+    
+    if response.status_code == 200:
+      dados = response.json()
+      
+      return JsonResponse(dados)
+    else:
+      return JsonResponse({'error': 'Erro ao fazer a chamada da API'}, status=500)
